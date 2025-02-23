066db0f2c3b11ade853a16252fa3bcb920ce5b4d
Marcelo Vieira <marcelodefy15@gmail.com>
2025-02-23 10:27:57 -0300

    feat - facebook pixel install

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { Button } from 'antd'
 import { Club } from './Pages/Club'
 import { Professionals } from './Pages/Professionals'
 import AOS from 'aos'
+import { Helmet } from 'react-helmet-async'
 
 export default function App() {
 
@@ -23,6 +24,31 @@ export default function App() {
 
   return (
     <div>
+      <Helmet>
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq) return; n=f.fbq=function() {
+                n.callMethod ?
+                n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+              };
+              if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
+              n.queue=[]; t=b.createElement(e); t.async=!0;
+              t.src=v; s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '445174198644146');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=445174198644146&ev=PageView&noscript=1" />`,
+          }}
+        />
+      </Helmet>
       <BrowserRouter basename='/'>
         <Header />
         <div className="page">
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 )
 
