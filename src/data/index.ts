49011c8a8fa339871f8ed6d4e7dec40d481fdc7f
marcelotvieira <marcelodefy15@gmail.com>
2025-08-05 16:19:32 -0300

    feat - plans

--- src/data/index.ts
+++ src/data/index.ts
@@ -15,19 +15,18 @@ import Cone from '../assets/images/cone.jpg'
 import Nasal from '../assets/images/Nasal.png'
 import limpeza from '../assets/images/limpeza-facial.jpg'
 
-
 type Service = {
-  name: string,
-  image: any
-  description: string,
-  duration: number,
-  icon: string,
-}
+  name: string;
+  image: any;
+  description: string;
+  duration: number;
+  icon: string;
+};
 
 type Partner = {
-  name: string,
-  image: any,
-}
+  name: string;
+  image: any;
+};
 
 const diegoPrices = [
   { name: 'Corte com Nery', value: 50 },
@@ -44,18 +43,21 @@ const diegoPrices = [
   { name: 'Alisamento capilar', value: 44.9 },
   { name: 'Selagem', value: 74.9 },
   { name: 'Luzes', value: 84.9 },
-  { name: 'Depilação nasal', value: 24.90 },
+  { name: 'Depilação nasal', value: 24.9 },
   { name: 'Terapia Cone hindu', value: 44.99 },
-  { name: 'Massagem facial express', value: 19.90 },
-  { name: 'Limpeza facial premium', value: 54.90 },
+  { name: 'Massagem facial express', value: 19.9 },
+  { name: 'Limpeza facial premium', value: 54.9 },
 ]
 
-  
-export const samuelPedroPrices = [{ name: 'Corte', value: 45 }, ...(diegoPrices
-  .map((s, i) => ({
-    ...s,
-    value: i === 1 ? s.value - 5 : s.value 
-  })).slice(1))]
+export const samuelPedroPrices = [
+  { name: 'Corte', value: 45 },
+  ...diegoPrices
+    .map((s, i) => ({
+      ...s,
+      value: i === 1 ? s.value - 5 : s.value,
+    }))
+    .slice(1),
+]
 
 export const ramonPrices = samuelPedroPrices
 
@@ -63,69 +65,70 @@ export const samuelTadeuPrices = samuelPedroPrices
 
 export const igorPrices = samuelPedroPrices
 
-
-export const variablePrices = [
-  'Luzes',
-  'Selagem',
-  'Alisamento capilar',
-]
-
-
+export const variablePrices = ['Luzes', 'Selagem', 'Alisamento capilar']
 
 export const services: Service[] = [
   {
     name: 'Corte',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Transforme seu visual com um corte de cabelo feito sob medida para realçar sua personalidade.',
+    description:
+      'Transforme seu visual com um corte de cabelo feito sob medida para realçar sua personalidade.',
     image: corte,
     duration: 40,
   },
   {
     name: 'Barba Express',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Agilidade e estilo em um serviço rápido e preciso de barbear.',
+    description:
+      'Agilidade e estilo em um serviço rápido e preciso de barbear.',
     image: barba,
     duration: 40,
   },
   {
     name: 'Barboterapia (Ozônio)',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Relaxe enquanto cuidamos da sua barba, proporcionando uma experiência revigorante.',
+    description:
+      'Relaxe enquanto cuidamos da sua barba, proporcionando uma experiência revigorante.',
     image: barboterapia,
     duration: 40,
   },
   {
     name: 'Sobrancelha',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Destaque seu olhar com um serviço especializado em designer de sobrancelhas masculinas.',
+    description:
+      'Destaque seu olhar com um serviço especializado em designer de sobrancelhas masculinas.',
     image: sobrancelha,
     duration: 40,
   },
   {
     name: 'Acabamento (Pezinho)',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Refine seu estilo com um acabamento preciso, incluindo detalhes no contorno.',
+    description:
+      'Refine seu estilo com um acabamento preciso, incluindo detalhes no contorno.',
     image: Acabamento,
     duration: 40,
   },
   {
     name: 'Kids',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Cuidado e estilo para os pequenos, proporcionando cortes adaptados às crianças.',
+    description:
+      'Cuidado e estilo para os pequenos, proporcionando cortes adaptados às crianças.',
     image: kids,
     duration: 40,
   },
   {
     name: 'Hidratação cabelo',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Restaure o brilho e força dos seus cabelos com nossos tratamentos de hidratação profunda.',
+    description:
+      'Restaure o brilho e força dos seus cabelos com nossos tratamentos de hidratação profunda.',
     image: hidratacao,
     duration: 40,
   },
   {
     name: 'Hidratação barba',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Restaure a vitalidade da sua barba com nossos tratamentos de hidratação profunda.',
+    description:
+      'Restaure a vitalidade da sua barba com nossos tratamentos de hidratação profunda.',
     image: hidratacaoBarba,
     duration: 40,
   },
@@ -146,49 +149,52 @@ export const services: Service[] = [
   {
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
     name: 'Selagem',
-    description: 'Devolve o brilho, resistência e maciez para seus fios, alisando e fortalecendo seus cabelos.',
+    description:
+      'Devolve o brilho, resistência e maciez para seus fios, alisando e fortalecendo seus cabelos.',
     image: selagem,
     duration: 40,
   },
   {
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
     name: 'Alisamento capilar',
-    description: 'Alise e reduza o volume de seus fios, eliminando a necessidade do uso de chapinhas ou outros métodos de alisamentos temporários.',
+    description:
+      'Alise e reduza o volume de seus fios, eliminando a necessidade do uso de chapinhas ou outros métodos de alisamentos temporários.',
     image: alisamento,
     duration: 40,
   },
   {
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
     name: 'Terapia Cone Hindu',
-    description: 'Revitalize mente e corpo com a terapia cone hindu, uma experiência única de bem-estar.',
+    description:
+      'Revitalize mente e corpo com a terapia cone hindu, uma experiência única de bem-estar.',
     image: Cone,
     duration: 40,
   },
   {
     name: 'Depilação nasal',
     icon: require('../assets/svgs/barber-brush-svgrepo-com.svg'),
-    description: 'Utilizando técnicas seguras e eficazes, remova os pelos indesejados da narina, garantindo conforto e higiêne.',
+    description:
+      'Utilizando técnicas seguras e eficazes, remova os pelos indesejados da narina, garantindo conforto e higiêne.',
     image: Nasal,
     duration: 40,
   },
   {
     icon: require('../assets/svgs/barbershop-svgrepo-com.svg'),
     name: 'Luzes',
-    description: 'Ilumine seu visual com destaques precisos de cores que complementam seu estilo.',
+    description:
+      'Ilumine seu visual com destaques precisos de cores que complementam seu estilo.',
     image: Luzes,
     duration: 40,
   },
   {
     icon: require('../assets/svgs/barbershop-svgrepo-com.svg'),
     name: 'Limpeza Facial Premium',
-    description: 'Remove impurezas, controla a oleosidade e proporciona uma sensação de frescor e renovação.',
+    description:
+      'Remove impurezas, controla a oleosidade e proporciona uma sensação de frescor e renovação.',
     image: limpeza,
     duration: 40,
   },
 ]
-
-
-
 
 export const partners: Partner[] = [
   {
@@ -214,24 +220,25 @@ export const partners: Partner[] = [
 ]
 
 type Benefit = {
-  name: string
-  active: boolean
-}
+  name: string;
+  active: boolean;
+};
 
 type Plan = {
-  name: string
-  price: number
-  description: string
-  benefits: Benefit[]
-  source: string
-}
+  name: string;
+  price: number;
+  description: string;
+  benefits: Benefit[];
+  source: string;
+};
 
 export const plans: Plan[] = [
   {
-    name: 'Economy',
-    source: 'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/economy/19',
-    price: 89.90,
-    description: 'Escolha um plano que combine com você.',
+    name: 'Econômico',
+    source:
+      'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/economy/19',
+    price: 89.9,
+    description: 'Corte ilimitado Seg a Qua',
     benefits: [
       {
         name: 'Corte ilimitado (seg a quarta)',
@@ -277,29 +284,30 @@ export const plans: Plan[] = [
         name: 'Intransferível',
         active: true,
       },
-    ]
+    ],
   },
   {
-    name: 'Prime',
-    source: 'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/prime/18',
-    price: 144.90,
-    description: 'Escolha um plano que combina com você.',
+    name: 'Básico',
+    source:
+      'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/gold/15',
+    price: 119.9,
+    description: 'Corte ilimitado Seg a Sáb',
     benefits: [
       {
-        name: 'Corte ilimitado (seg a quarta)',
-        active: true,
+        name: 'Corte ilimitado (seg a sábado)',
+        active: false,
       },
       {
-        name: 'Barba ilimitada (seg a quarta)',
-        active: true,
+        name: 'Barba ilimitada (seg a sábado)',
+        active: false,
       },
       {
-        name: 'Acabamento pezinho (seg a quarta)',
-        active: true,
+        name: 'Acabamento pezinho (seg a sábado)',
+        active: false,
       },
       {
         name: 'Corte ilimitado',
-        active: false,
+        active: true,
       },
       {
         name: 'Barba Express ilimitada',
@@ -307,7 +315,7 @@ export const plans: Plan[] = [
       },
       {
         name: 'Acabamento pezinho ilimitado',
-        active: false,
+        active: true,
       },
       // {
       //   name: 'Desconto em Produtos e Serviços',
@@ -329,24 +337,79 @@ export const plans: Plan[] = [
         name: 'Intransferível',
         active: true,
       },
-    ]
+    ],
   },
   {
-    name: 'Gold',
-    source: 'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/gold/15',
-    price: 119.90,
-    description: 'Escolha um plano que combine com você.',
+    name: 'Intermediário',
+    source:
+      'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/prime/18',
+    price: 144.9,
+    description: 'Corte + Barba ilimitado Seg a Qua',
     benefits: [
       {
         name: 'Corte ilimitado (seg a quarta)',
-        active: false,
+        active: true,
       },
       {
         name: 'Barba ilimitada (seg a quarta)',
-        active: false,
+        active: true,
       },
       {
         name: 'Acabamento pezinho (seg a quarta)',
+        active: true,
+      },
+      {
+        name: 'Corte ilimitado',
+        active: false,
+      },
+      {
+        name: 'Barba Express ilimitada',
+        active: false,
+      },
+      {
+        name: 'Acabamento pezinho ilimitado',
+        active: false,
+      },
+      // {
+      //   name: 'Desconto em Produtos e Serviços',
+      //   active: true,
+      // },
+      {
+        name: 'Benefícios com mais de 30.000 Empresas',
+        active: true,
+      },
+      {
+        name: 'TV Gratuita no App',
+        active: true,
+      },
+      {
+        name: 'Sorteios Exclusivos',
+        active: true,
+      },
+      {
+        name: 'Intransferível',
+        active: true,
+      },
+    ],
+  },
+
+  {
+    name: 'Executivo',
+    source:
+      'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/black/17',
+    price: 219.9,
+    description: 'Corte + Barba ilimitado Seg a Sáb.',
+    benefits: [
+      {
+        name: 'Corte ilimitado (seg a sábado)',
+        active: false,
+      },
+      {
+        name: 'Barba ilimitada (seg a sábado)',
+        active: false,
+      },
+      {
+        name: 'Acabamento pezinho (seg a sábado)',
         active: false,
       },
       {
@@ -355,7 +418,7 @@ export const plans: Plan[] = [
       },
       {
         name: 'Barba Express ilimitada',
-        active: false,
+        active: true,
       },
       {
         name: 'Acabamento pezinho ilimitado',
@@ -381,76 +444,25 @@ export const plans: Plan[] = [
         name: 'Intransferível',
         active: true,
       },
-    ]
+    ],
   },
   {
-    name: 'Black',
-    source: 'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/black/17',
-    price: 219.90,
-    description: 'Escolha um plano que combine com você.',
+    name: 'Essencial',
+    source:
+      'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/platinum/16',
+    price: 109.9,
+    description: 'Barba Express ilimitada',
     benefits: [
-      {
-        name: 'Corte ilimitado (seg a quarta)',
-        active: false,
-      },
-      {
-        name: 'Barba ilimitada (seg a quarta)',
-        active: false,
-      },
-      {
-        name: 'Acabamento pezinho (seg a quarta)',
-        active: false,
-      },
       {
         name: 'Corte ilimitado',
-        active: true,
-      },
-      {
-        name: 'Barba Express ilimitada',
-        active: true,
-      },
-      {
-        name: 'Acabamento pezinho ilimitado',
-        active: true,
-      },
-      // {
-      //   name: 'Desconto em Produtos e Serviços',
-      //   active: true,
-      // },
-      {
-        name: 'Benefícios com mais de 30.000 Empresas',
-        active: true,
-      },
-      {
-        name: 'TV Gratuita no App',
-        active: true,
-      },
-      {
-        name: 'Sorteios Exclusivos',
-        active: true,
-      },
-      {
-        name: 'Intransferível',
-        active: true,
-      },
-    ]
-  },
-  {
-    name: 'Platinum',
-    source: 'https://celcash.celcoin.com.br/landingpage3865984/principal/assinar/platinum/16',
-    price: 109.90,
-    description: 'Escolha um plano que combine com você.',
-    benefits: [
-      {
-        name: 'Corte ilimitado (seg a quarta)',
         active: false,
       },
       {
-        name: 'Barba ilimitada (seg a quarta)',
+        name: 'Barba ilimitada',
         active: false,
       },
       {
-        name: 'Acabamento pezinho (seg a quarta)',
+        name: 'Acabamento pezinho',
         active: false,
       },
       {
@@ -485,17 +497,17 @@ export const plans: Plan[] = [
         name: 'Intransferível',
         active: true,
       },
-    ]
+    ],
   },
 ]
 
 type Professional = {
-  firstName: string
-  lastName: string
-  picture: any
-  portfolio: any[]
-  priceTable: { name: string, value: number }[]
-}
+  firstName: string;
+  lastName: string;
+  picture: any;
+  portfolio: any[];
+  priceTable: { name: string; value: number }[];
+};
 
 export const professionals: Professional[] = [
   // {
@@ -511,65 +523,50 @@ export const professionals: Professional[] = [
     firstName: 'Eric',
     lastName: 'Junio',
     picture: require('../assets/images/professionals/eric.jpg'),
-    portfolio: [
-
-    ],
-    priceTable: samuelTadeuPrices
+    portfolio: [],
+    priceTable: samuelTadeuPrices,
   },
   {
     firstName: 'Samuel',
     lastName: 'Pedro',
     picture: require('../assets/images/professionals/samuelpedro1.jpg'),
-    portfolio: [
-
-    ],
-    priceTable: samuelPedroPrices
+    portfolio: [],
+    priceTable: samuelPedroPrices,
   },
   {
     firstName: 'Igor',
     lastName: 'Augustus',
     picture: require('../assets/images/professionals/igor1.jpg'),
-    portfolio: [
-
-    ],
-    priceTable: igorPrices
-
+    portfolio: [],
+    priceTable: igorPrices,
   },
   {
     firstName: 'Ramon',
     lastName: 'Eduardo',
     picture: require('../assets/images/professionals/ramon1.jpg'),
-    portfolio: [
-
-    ],
-    priceTable: ramonPrices
-
+    portfolio: [],
+    priceTable: ramonPrices,
   },
   {
     firstName: 'Janielle',
     lastName: 'Paixão',
     picture: require('../assets/images/professionals/janielle1.jpg'),
-    portfolio: [
-    ],
-    priceTable: samuelTadeuPrices
+    portfolio: [],
+    priceTable: samuelTadeuPrices,
   },
   {
     firstName: 'Gabriel',
     lastName: 'Rodrigues',
     picture: require('../assets/images/professionals/gabriel1.jpg'),
-    portfolio: [
-
-    ],
-    priceTable: ramonPrices
-
+    portfolio: [],
+    priceTable: ramonPrices,
   },
   {
     firstName: 'Pierre',
     lastName: 'Rocha',
     picture: require('../assets/images/professionals/pierre1.jpg'),
-    portfolio: [
-    ],
-    priceTable: samuelTadeuPrices
+    portfolio: [],
+    priceTable: samuelTadeuPrices,
   },
 ]
 
